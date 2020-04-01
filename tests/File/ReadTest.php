96224f201a0e46ae3a12ae40eaa640084afb5f44
Richard Weinhold <privat@ricwein.com>
2020-04-01 15:15:42 +0200

    adds tests for lines file-reading

--- tests/File/ReadTest.php
+++ tests/File/ReadTest.php
@@ -5,6 +5,12 @@ declare(strict_types=1);
 namespace ricwein\FileSystem\Tests\File;
 
 use PHPUnit\Framework\TestCase;
+use ricwein\FileSystem\Exceptions\AccessDeniedException;
+use ricwein\FileSystem\Exceptions\ConstraintsException;
+use ricwein\FileSystem\Exceptions\Exception;
+use ricwein\FileSystem\Exceptions\FileNotFoundException;
+use ricwein\FileSystem\Exceptions\RuntimeException;
+use ricwein\FileSystem\Exceptions\UnexpectedValueException;
 use ricwein\FileSystem\File;
 use ricwein\FileSystem\Storage;
 use ricwein\FileSystem\Helper\Constraint;
@@ -18,6 +24,13 @@ class ReadTest extends TestCase
 {
     /**
      * @return void
+     * @throws AccessDeniedException
+     * @throws ConstraintsException
+     * @throws Exception
+     * @throws FileNotFoundException
+     * @throws RuntimeException
+     * @throws UnexpectedValueException
+     * @throws \Exception
      */
     public function testFileRead()
     {
@@ -36,13 +49,45 @@ class ReadTest extends TestCase
     }
 
     /**
+     * @throws AccessDeniedException
+     * @throws Exception
+     * @throws RuntimeException
+     * @throws UnexpectedValueException
+     * @throws \Exception
+     */
+    public function testLineRead()
+    {
+        $file = new File(new Storage\Disk(__DIR__ . '/../_examples', 'test.txt'), Constraint::STRICT & ~Constraint::IN_SAFEPATH);
+        $this->assertSame(
+            $file->readAsLines(),
+            file(__DIR__ . '/../_examples/test.txt')
+        );
+
+        $message = bin2hex(random_bytes(2 ** 10));
+        $message = chunk_split($message, 8, PHP_EOL);
+
+        $file = new File(new Storage\Memory($message));
+        $this->assertSame(
+            $file->readAsLines(),
+            explode(PHP_EOL, $message)
+        );
+    }
+
+    /**
      * @return void
+     * @throws AccessDeniedException
+     * @throws ConstraintsException
+     * @throws Exception
+     * @throws FileNotFoundException
+     * @throws RuntimeException
+     * @throws UnexpectedValueException
+     * @throws \Exception
      */
     public function testPartialFileRead()
     {
         $file = new File(new Storage\Disk(__DIR__ . '/../_examples', 'test.txt'), Constraint::STRICT & ~Constraint::IN_SAFEPATH);
 
-        $length = (int) floor($file->getSize() / 2);
+        $length = (int)floor($file->getSize() / 2);
         $this->assertTrue(!empty($length));
         $this->assertSame(
             $file->read(0, $length),
@@ -52,7 +97,7 @@ class ReadTest extends TestCase
         $message = bin2hex(random_bytes(2 ** 10));
         $file = new File(new Storage\Memory($message));
 
-        $length = (int) floor($file->getSize() / 2);
+        $length = (int)floor($file->getSize() / 2);
         $this->assertTrue(!empty($length));
         $this->assertSame(
             $file->read(0, $length),
@@ -62,6 +107,12 @@ class ReadTest extends TestCase
 
     /**
      * @return void
+     * @throws AccessDeniedException
+     * @throws ConstraintsException
+     * @throws Exception
+     * @throws FileNotFoundException
+     * @throws RuntimeException
+     * @throws UnexpectedValueException
      */
     public function testFileMimeTypeGuessing()
     {
